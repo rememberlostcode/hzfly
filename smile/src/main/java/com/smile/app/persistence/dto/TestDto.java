45d00f0d063dc2dc1511276dfa130ca2f6d018de
huoding <hdhd0000@sina.com>
2014-03-27 10:29:54 +0800

    WEB

---
+++ smile/src/main/java/com/smile/app/persistence/dto/TestDto.java
@@ -0,0 +1,22 @@
+package com.smile.app.persistence.dto;
+/**
+ * 测试Dto
+ * @author fuwb 20140326 
+ */
+public class TestDto {
+	private Integer id;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ smile/src/main/java/com/smile/app/persistence/entity/TestModel.java
@@ -0,0 +1,22 @@
+package com.smile.app.persistence.entity;
+/**
+ * 测试Model
+ * @author fuwb 20140326 
+ */
+public class TestModel {
+	private Integer id;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
